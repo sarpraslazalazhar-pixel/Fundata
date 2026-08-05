f208810bcebf2788839748d7edd5786e032cf5e3
LAZ AL AZHAR <sarpraslazalazhar@gmail.com>
2026-08-05 16:20:08 +0700

    fix cat

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,11 +9,12 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
- broadcaster: 'reverb',
- key: import.meta.env.VITE_REVERB_APP_KEY,
- wsHost: import.meta.env.VITE_REVERB_HOST === 'localhost' ? window.location.hostname : import.meta.env.VITE_REVERB_HOST,
- wsPort: import.meta.env.VITE_REVERB_PORT,
- wssPort: import.meta.env.VITE_REVERB_PORT,
- forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
- enabledTransports: ['ws', 'wss'],
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1'}.pusher.com`,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
